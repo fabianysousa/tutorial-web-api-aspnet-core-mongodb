8bd4e904068e9de3a2e63c366f7cb8a4daf6a8e7
Fabiany Sousa <fabianydesousa@gmail.com>
2020-11-10 14:32:25 -0300

    Corrigindo lauchSettings.json e adicionando rota no BooksController

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BooksApi.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class BooksController : ControllerBase
     {
         private readonly BookService _bookService;
